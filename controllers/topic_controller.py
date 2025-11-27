e87328daa7fd9de0504e95fcd1327c3d9b9b95f7
Gabriel <gabrielrdaraujos@gmail.com>
2025-11-27 10:22:19 -0300

    chore: inclusao de topics no merge das rotas

---
+++ controllers/topic_controller.py
@@ -0,0 +1,131 @@
+# controllers/topic_controller.py
+from bottle import Bottle, request
+from .base_controller import BaseController
+from services.topic_service import TopicService
+from services.subject_service import SubjectService
+from controllers.auth_controller import AuthController
+
+
+class TopicController(BaseController):
+    def __init__(self, app):
+        super().__init__(app)
+        self.topic_service = TopicService()
+        self.subject_service = SubjectService()
+        self._setup_routes()
+
+    def _setup_routes(self):
+        # Listar tópicos de uma matéria
+        self.app.route(
+            '/subjects/<subject_id:int>/topics',
+            method='GET',
+            callback=self.list_by_subject
+        )
+
+        # Criar novo tópico
+        self.app.route(
+            '/subjects/<subject_id:int>/topics/new',
+            method=['GET', 'POST'],
+            callback=self.add
+        )
+
+        # Editar tópico existente
+        self.app.route(
+            '/subjects/<subject_id:int>/topics/<topic_id:int>/edit',
+            method=['GET', 'POST'],
+            callback=self.edit
+        )
+
+        # Excluir tópico
+        self.app.route(
+            '/subjects/<subject_id:int>/topics/<topic_id:int>/delete',
+            method='POST',
+            callback=self.delete
+        )
+
+    # Helpers
+    def _get_logged_user_id(self):
+        """Obtém o ID do usuário logado usando o AuthController."""
+        return AuthController.get_logged_user_id()
+
+    def _ensure_subject_owner(self, subject_id: int):
+        """Garante que o subject pertence ao usuário logado."""
+        user_id = self._get_logged_user_id()
+        if not user_id:
+            return self.redirect('/login')
+
+        subject = self.subject_service.get_by_id(subject_id)
+        if not subject or subject.user_id != user_id:
+            # não deixa acessar matéria de outro usuário
+            return self.redirect('/subjects')
+
+        return subject
+
+    # Actions
+    def list_by_subject(self, subject_id):
+        subject = self._ensure_subject_owner(subject_id)
+        if not isinstance(subject, object):  # já houve redirect
+            return subject
+
+        all_topics = self.topic_service.list_all()
+        topics = [t for t in all_topics if t.subject_id == subject_id]
+
+        # topics.tpl ainda será criado pelo Dev 2
+        return self.render(
+            'topics',
+            subject=subject,
+            topics=topics
+        )
+
+    def add(self, subject_id):
+        subject = self._ensure_subject_owner(subject_id)
+        if not isinstance(subject, object):
+            return subject
+
+        if request.method == 'GET':
+            # topic_form.tpl será criado pelo Dev 2
+            return self.render(
+                'topic_form',
+                subject=subject,
+                topic=None,
+                action=f'/subjects/{subject_id}/topics/new'
+            )
+
+        # POST
+        self.topic_service.save()
+        return self.redirect(f'/subjects/{subject_id}/topics')
+
+    def edit(self, subject_id, topic_id):
+        subject = self._ensure_subject_owner(subject_id)
+        if not isinstance(subject, object):
+            return subject
+
+        topic = self.topic_service.get_by_id(topic_id)
+        if not topic or topic.subject_id != subject_id:
+            return self.redirect(f'/subjects/{subject_id}/topics')
+
+        if request.method == 'GET':
+            return self.render(
+                'topic_form',
+                subject=subject,
+                topic=topic,
+                action=f'/subjects/{subject_id}/topics/{topic_id}/edit'
+            )
+
+        # POST
+        self.topic_service.update(topic)
+        return self.redirect(f'/subjects/{subject_id}/topics')
+
+    def delete(self, subject_id, topic_id):
+        subject = self._ensure_subject_owner(subject_id)
+        if not isinstance(subject, object):
+            return subject
+
+        topic = self.topic_service.get_by_id(topic_id)
+        if topic and topic.subject_id == subject_id:
+            self.topic_service.delete(topic_id)
+
+        return self.redirect(f'/subjects/{subject_id}/topics')
+
+
+topic_routes = Bottle()
+topic_controller = TopicController(topic_routes)
