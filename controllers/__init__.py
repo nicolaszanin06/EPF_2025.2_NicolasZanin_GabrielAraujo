121e4fe9c9efbcc8b3df26b9148acd879bb3f268
Gabriel <gabrielrdaraujos@gmail.com>
2025-11-30 11:47:24 -0300

    feat: criacao das estatisticas de sessoes e contagem de materias/topicos

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -2,12 +2,15 @@ from bottle import Bottle
 from controllers.user_controller import user_routes
 from controllers.auth_controller import auth_routes
 from controllers.subject_controller import subject_routes
-from controllers.study_session_controller import session_routes
 from controllers.topic_controller import topic_routes
+from controllers.study_session_controller import session_routes
+from controllers.stats_controller import stats_routes
+
 
 def init_controllers(app: Bottle):
     app.merge(auth_routes)
     app.merge(subject_routes)
-    app.merge(session_routes)
-    app.merge(user_routes)
     app.merge(topic_routes)
+    app.merge(session_routes)
+    app.merge(stats_routes)
+    app.merge(user_routes)
