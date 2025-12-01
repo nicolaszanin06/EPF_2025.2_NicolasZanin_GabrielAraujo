363231210f2f518a9bba63f1eb6df1753d3896f6
Gabriel <gabrielrdaraujos@gmail.com>
2025-11-30 22:27:37 -0300

    feat: criacao do painel de controle que apresenta todas as informacoes

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -5,14 +5,14 @@ from controllers.subject_controller import subject_routes
 from controllers.topic_controller import topic_routes
 from controllers.study_session_controller import session_routes
 from controllers.stats_controller import stats_routes
-from controllers.stats_controller import stats_routes
+from controllers.dashboard_controller import dashboard_routes
 
-
-def init_controllers(app: Bottle):
+def init_controllers(app):
     app.merge(auth_routes)
     app.merge(subject_routes)
     app.merge(topic_routes)
     app.merge(session_routes)
     app.merge(stats_routes)
+    app.merge(dashboard_routes)   
     app.merge(user_routes)
-    app.merge(stats_routes)
+
